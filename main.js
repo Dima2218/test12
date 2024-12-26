2c36b0b63cff83d5766043b8e8e56a68fe841349
Dima2218 <dimapetrik1974@gmail.com>
2024-12-26 13:09:06 +0200

    third commit

---
+++ main.js
@@ -0,0 +1,13 @@
+fetch('https://localhost:5000/api/user/')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); 
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
